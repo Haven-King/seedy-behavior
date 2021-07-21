b049215a0d2d24c729fc7b3e5d4e9b07ec1d6f3a
Haven <haven@hephaestus.dev>
2021-07-21 10:58:12 -0700

    Replace redirects with ModifyVariables.

--- src/main/java/dev/hephaestus/seedy/mixin/world/biome/source/MultiNoiseBiomeSourceMixin.java
+++ src/main/java/dev/hephaestus/seedy/mixin/world/biome/source/MultiNoiseBiomeSourceMixin.java
@@ -1,48 +1,33 @@
 package dev.hephaestus.seedy.mixin.world.biome.source;
 
-import com.mojang.datafixers.util.Pair;
 import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.PrimitiveCodec;
 import dev.hephaestus.seedy.SeedSupplier;
-import net.minecraft.class_6452;
-import net.minecraft.util.registry.Registry;
-import net.minecraft.world.biome.Biome;
 import net.minecraft.world.biome.source.MultiNoiseBiomeSource;
-import net.minecraft.world.gen.ChunkRandom;
 import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-import java.util.List;
-import java.util.Optional;
-import java.util.function.Supplier;
 
 @Mixin(MultiNoiseBiomeSource.class)
 public class MultiNoiseBiomeSourceMixin {
-	@Shadow private long seed;
 	@Unique private static long LAST_SEED = SeedSupplier.MARKER;
 
-	@Redirect(method = "method_30827(Lcom/mojang/serialization/codecs/RecordCodecBuilder$Instance;)Lcom/mojang/datafixers/kinds/App;", at = @At(value = "INVOKE", target = "Lcom/mojang/serialization/codecs/PrimitiveCodec;fieldOf(Ljava/lang/String;)Lcom/mojang/serialization/MapCodec;", ordinal = 0))
+	@Redirect(method = "method_37688(Lcom/mojang/serialization/codecs/RecordCodecBuilder$Instance;)Lcom/mojang/datafixers/kinds/App;", at = @At(value = "INVOKE", target = "Lcom/mojang/serialization/codecs/PrimitiveCodec;fieldOf(Ljava/lang/String;)Lcom/mojang/serialization/MapCodec;", ordinal = 0))
 	private static MapCodec<Long> giveUsRandomSeeds(PrimitiveCodec<Long> codec, final String name) {
 		return codec.fieldOf(name).orElseGet(SeedSupplier::getSeed);
 	}
 
-	@Inject(method = "<init>(JLnet/minecraft/class_6452$class_6455;Lnet/minecraft/world/biome/source/MultiNoiseBiomeSource$NoiseParameters;Lnet/minecraft/world/biome/source/MultiNoiseBiomeSource$NoiseParameters;Lnet/minecraft/world/biome/source/MultiNoiseBiomeSource$NoiseParameters;Lnet/minecraft/world/biome/source/MultiNoiseBiomeSource$NoiseParameters;Lnet/minecraft/world/biome/source/MultiNoiseBiomeSource$NoiseParameters;IIZLjava/util/Optional;)V", at = @At(value = "FIELD", target = "Lnet/minecraft/world/biome/source/MultiNoiseBiomeSource;seed:J", opcode = Opcodes.PUTFIELD, shift = At.Shift.AFTER))
-	private void replaceSeed(long l, class_6452.class_6455<Biome> arg, MultiNoiseBiomeSource.NoiseParameters noiseParameters, MultiNoiseBiomeSource.NoiseParameters noiseParameters2, MultiNoiseBiomeSource.NoiseParameters noiseParameters3, MultiNoiseBiomeSource.NoiseParameters noiseParameters4, MultiNoiseBiomeSource.NoiseParameters noiseParameters5, int i, int j, boolean bl, Optional<Pair<Registry<Biome>, MultiNoiseBiomeSource.Preset>> optional, CallbackInfo ci) {
-		if (this.seed == SeedSupplier.MARKER) {
-			this.seed = LAST_SEED;
+	@ModifyVariable(method = "<init>(JLnet/minecraft/class_6452$class_6455;Lnet/minecraft/world/biome/source/MultiNoiseBiomeSource$NoiseParameters;Lnet/minecraft/world/biome/source/MultiNoiseBiomeSource$NoiseParameters;Lnet/minecraft/world/biome/source/MultiNoiseBiomeSource$NoiseParameters;Lnet/minecraft/world/biome/source/MultiNoiseBiomeSource$NoiseParameters;Lnet/minecraft/world/biome/source/MultiNoiseBiomeSource$NoiseParameters;IIZLjava/util/Optional;)V", at = @At(value = "FIELD", target = "Lnet/minecraft/world/biome/source/MultiNoiseBiomeSource;field_34194:Lnet/minecraft/class_6466;", opcode = Opcodes.PUTFIELD, shift = At.Shift.AFTER), ordinal = 0)
+	private long replaceSeed(long seed) {
+		if (seed == SeedSupplier.MARKER) {
+			return LAST_SEED;
 		} else {
 			LAST_SEED = seed;
 		}
-	}
 
-	@Redirect(method = "<init>(JLnet/minecraft/class_6452$class_6455;Lnet/minecraft/world/biome/source/MultiNoiseBiomeSource$NoiseParameters;Lnet/minecraft/world/biome/source/MultiNoiseBiomeSource$NoiseParameters;Lnet/minecraft/world/biome/source/MultiNoiseBiomeSource$NoiseParameters;Lnet/minecraft/world/biome/source/MultiNoiseBiomeSource$NoiseParameters;Lnet/minecraft/world/biome/source/MultiNoiseBiomeSource$NoiseParameters;IIZLjava/util/Optional;)V", at = @At(value = "NEW", target = "net/minecraft/world/gen/ChunkRandom"))
-	private ChunkRandom useOurSeed(long seed) {
-		return new ChunkRandom(this.seed);
+		return seed;
 	}
 }
--- src/main/java/dev/hephaestus/seedy/mixin/world/gen/chunk/NoiseChunkGeneratorMixin.java
+++ src/main/java/dev/hephaestus/seedy/mixin/world/gen/chunk/NoiseChunkGeneratorMixin.java
@@ -4,25 +4,18 @@ import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.PrimitiveCodec;
 import dev.hephaestus.seedy.SeedSupplier;
 import net.minecraft.world.biome.source.BiomeSource;
-import net.minecraft.world.gen.ChunkRandom;
 import net.minecraft.world.gen.chunk.ChunkGenerator;
-import net.minecraft.world.gen.chunk.ChunkGeneratorSettings;
 import net.minecraft.world.gen.chunk.NoiseChunkGenerator;
 import net.minecraft.world.gen.chunk.StructuresConfig;
 import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-import java.util.function.Supplier;
 
 @Mixin(NoiseChunkGenerator.class)
 public abstract class NoiseChunkGeneratorMixin extends ChunkGenerator {
-	@Shadow public long seed;
 	@Unique private static long LAST_SEED = SeedSupplier.MARKER;
 
 	public NoiseChunkGeneratorMixin(BiomeSource biomeSource, StructuresConfig structuresConfig) {
@@ -34,18 +27,14 @@ public abstract class NoiseChunkGeneratorMixin extends ChunkGenerator {
 		return codec.fieldOf(name).orElseGet(SeedSupplier::getSeed);
 	}
 
-	@Inject(method = "<init>(Lnet/minecraft/world/biome/source/BiomeSource;Lnet/minecraft/world/biome/source/BiomeSource;JLjava/util/function/Supplier;)V", at = @At(value = "FIELD", target = "Lnet/minecraft/world/gen/chunk/NoiseChunkGenerator;seed:J", opcode = Opcodes.PUTFIELD, shift = At.Shift.AFTER))
-	private void replaceSeed(BiomeSource populationSource, BiomeSource biomeSource, long seed, Supplier<ChunkGeneratorSettings> settings, CallbackInfo ci) {
+	@ModifyVariable(method = "<init>(Lnet/minecraft/world/biome/source/BiomeSource;Lnet/minecraft/world/biome/source/BiomeSource;JLjava/util/function/Supplier;)V", at = @At(value = "FIELD", target = "Lnet/minecraft/world/gen/chunk/NoiseChunkGenerator;seed:J", opcode = Opcodes.PUTFIELD, shift = At.Shift.AFTER), ordinal = 0)
+	private long replaceSeed(long seed) {
 		if (seed == SeedSupplier.MARKER) {
-			this.seed = LAST_SEED;
-			this.worldSeed = LAST_SEED;
+			return LAST_SEED;
 		} else {
 			LAST_SEED = seed;
 		}
-	}
 
-	@Redirect(method = "<init>(Lnet/minecraft/world/biome/source/BiomeSource;Lnet/minecraft/world/biome/source/BiomeSource;JLjava/util/function/Supplier;)V", at = @At(value = "NEW", target = "net/minecraft/world/gen/ChunkRandom"))
-	private ChunkRandom useOurSeed(long seed) {
-		return new ChunkRandom(this.seed);
+		return seed;
 	}
 }
