8b6ab2ab14ab3c03fa87e6f3e06a48467ecada4b
Haven <haven@hephaestus.dev>
2021-08-09 09:33:43 -0700

    Replace redirects with ModifyVariables.

--- src/main/java/dev/hephaestus/seedy/mixin/world/biome/source/MultiNoiseBiomeSourceMixin.java
+++ src/main/java/dev/hephaestus/seedy/mixin/world/biome/source/MultiNoiseBiomeSourceMixin.java
@@ -1,25 +1,16 @@
 package dev.hephaestus.seedy.mixin.world.biome.source;
 
-import com.mojang.datafixers.util.Pair;
 import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.PrimitiveCodec;
 import dev.hephaestus.seedy.SeedSupplier;
-import net.minecraft.util.registry.Registry;
-import net.minecraft.world.biome.Biome;
 import net.minecraft.world.biome.source.MultiNoiseBiomeSource;
-import net.minecraft.world.gen.ChunkRandom;
 import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-import java.util.List;
-import java.util.Optional;
-import java.util.function.Supplier;
 
 @Mixin(MultiNoiseBiomeSource.class)
 public class MultiNoiseBiomeSourceMixin {
@@ -31,17 +22,14 @@ public class MultiNoiseBiomeSourceMixin {
 		return codec.fieldOf(name).orElseGet(SeedSupplier::getSeed);
 	}
 
-	@Inject(method = "<init>(JLjava/util/List;Lnet/minecraft/world/biome/source/MultiNoiseBiomeSource$NoiseParameters;Lnet/minecraft/world/biome/source/MultiNoiseBiomeSource$NoiseParameters;Lnet/minecraft/world/biome/source/MultiNoiseBiomeSource$NoiseParameters;Lnet/minecraft/world/biome/source/MultiNoiseBiomeSource$NoiseParameters;Ljava/util/Optional;)V", at = @At(value = "FIELD", target = "Lnet/minecraft/world/biome/source/MultiNoiseBiomeSource;seed:J", opcode = Opcodes.PUTFIELD, shift = At.Shift.AFTER))
-	private void replaceSeed(long seed, List<Pair<Biome.MixedNoisePoint, Supplier<Biome>>> biomePoints, MultiNoiseBiomeSource.NoiseParameters temperatureNoiseParameters, MultiNoiseBiomeSource.NoiseParameters humidityNoiseParameters, MultiNoiseBiomeSource.NoiseParameters altitudeNoiseParameters, MultiNoiseBiomeSource.NoiseParameters weirdnessNoiseParameters, Optional<Pair<Registry<Biome>, MultiNoiseBiomeSource.Preset>> instance, CallbackInfo ci) {
+	@ModifyVariable(method = "<init>(JLjava/util/List;Lnet/minecraft/world/biome/source/MultiNoiseBiomeSource$NoiseParameters;Lnet/minecraft/world/biome/source/MultiNoiseBiomeSource$NoiseParameters;Lnet/minecraft/world/biome/source/MultiNoiseBiomeSource$NoiseParameters;Lnet/minecraft/world/biome/source/MultiNoiseBiomeSource$NoiseParameters;Ljava/util/Optional;)V", at = @At(value = "FIELD", target = "Lnet/minecraft/world/biome/source/MultiNoiseBiomeSource;seed:J", opcode = Opcodes.PUTFIELD, shift = At.Shift.AFTER), ordinal = 0)
+	private long replaceSeed(long seed) {
 		if (seed == SeedSupplier.MARKER) {
-			this.seed = LAST_SEED;
+			return LAST_SEED;
 		} else {
 			LAST_SEED = seed;
 		}
-	}
 
-	@Redirect(method = "<init>(JLjava/util/List;Lnet/minecraft/world/biome/source/MultiNoiseBiomeSource$NoiseParameters;Lnet/minecraft/world/biome/source/MultiNoiseBiomeSource$NoiseParameters;Lnet/minecraft/world/biome/source/MultiNoiseBiomeSource$NoiseParameters;Lnet/minecraft/world/biome/source/MultiNoiseBiomeSource$NoiseParameters;Ljava/util/Optional;)V", at = @At(value = "NEW", target = "net/minecraft/world/gen/ChunkRandom"))
-	private ChunkRandom useOurSeed(long seed) {
-		return new ChunkRandom(this.seed);
+		return seed;
 	}
 }
